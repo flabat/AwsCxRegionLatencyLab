4d2d8aedd8ffa4a81d70b23ff310b1c8fbc70be4
Fabian Labat <labatf@amazon.com>
2022-02-26 06:30:04 -0500

    remove gitignore files

--- lib/aws-cx-region-latency-lab-stack.ts
+++ lib/aws-cx-region-latency-lab-stack.ts
@@ -1,16 +1,75 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
+import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { TextWidget } from 'aws-cdk-lib/aws-cloudwatch';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+interface AwsCxRegionLatencyLabStackProps extends StackProps {
+  vpccidr?: string;
+  remotecidr?: string;
+}
+
+
 export class AwsCxRegionLatencyLabStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props?: AwsCxRegionLatencyLabStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      cidr: props?.vpccidr,
+      maxAzs: 3
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCxRegionLatencyLabQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const vpcPrivateSubnets = vpc.privateSubnets.slice(0,3).map(x => x.subnetId);
+
+    
+
+    const sg = new ec2.SecurityGroup(this, 'SG', {
+      vpc
+    })
+
+    sg.addIngressRule(ec2.Peer.ipv4('10.0.0.0/8'), ec2.Port.allTraffic())
+
+    const role = new iam.Role(this, 'Role', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')
+    })
+
+    role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'))
+
+
+    const asg = new autoscaling.AutoScalingGroup(this, 'ASG', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.COMPUTE5, ec2.InstanceSize.LARGE),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
+      }),
+      minCapacity: 3,
+      maxCapacity: 6,
+      securityGroup: sg,
+      role: role
+    })
+
+    const tgw = new ec2.CfnTransitGateway(this, 'TGW')
+
+    const cfnTransitGatewayAttachment = new ec2.CfnTransitGatewayAttachment(this, 'MyCfnTransitGatewayAttachment', {
+      subnetIds: vpcPrivateSubnets,
+      transitGatewayId: tgw.attrId,
+      vpcId: vpc.vpcId,
+    });
+
+    vpc.privateSubnets.forEach(({ routeTable: { routeTableId } }, index) => {
+      
+      const route = new ec2.CfnRoute(this, 'PrivateSubnetTGWRoute' + index, {
+        destinationCidrBlock: props?.remotecidr,
+        routeTableId,
+        transitGatewayId: tgw.attrId
+      })
+      route.node.addDependency(tgw)
+    })
+
+    
+    
   }
 }
+
---
+++ test.py
@@ -0,0 +1,106 @@
+
+#!/usr/bin/env python3
+"""
+TCP Ping Test (defaults to port 80, 10000 packets)
+ 
+Usage: ./tcpping.py host [port] [maxCount]
+- Ctrl-C Exits with Results
+"""
+ 
+import sys
+import socket
+import time
+# import signal
+from timeit import default_timer as timer
+from datetime import datetime, timedelta
+import random
+ 
+host = None
+port = 80
+ 
+# Default to 10000 connections max
+maxCount = 10000
+count = 0
+ 
+# Pass/Fail counters
+passed = 0
+failed = 0
+ 
+
+# PUT REMOTE HOSTS HERE
+remote_hosts=[
+    '10.20.147.136',
+    '10.20.180.3',
+    '10.20.125.232',
+    '10.40.169.255',
+    '10.40.132.216',
+    '10.40.111.12'
+]
+port=80
+ 
+script_start = datetime.now()
+script_end = datetime.now() + timedelta(seconds=600) # 10 minutes
+ 
+
+# Loop while less than max count or until Ctrl-C caught
+while count < maxCount:
+    if datetime.now() > script_end:
+        sys.exit(0)
+ 
+    # Increment Counter
+    count += 1
+ 
+    success = False
+ 
+    # New Socket
+    s = socket.socket(
+    socket.AF_INET, socket.SOCK_STREAM)
+ 
+    # 10sec Timeout
+    s.settimeout(10)
+ 
+    # Start a timer
+    s_start = timer()
+    start = datetime.now()
+ 
+    start_time = start.strftime("%Y-%m-%dT%H:%M:%S.%f %Z")
+ 
+    #get a random host
+    host=remote_hosts[random.randrange(0, len(remote_hosts))]
+    # Try to Connect
+    try:
+        s.connect((host, int(port)))
+        s.shutdown(socket.SHUT_RD)
+        success = True
+   
+    # Connection Timed Out
+    except socket.timeout:
+        print("Connection timed out!")
+        failed += 1
+    except OSError as e:
+        print("OS Error:", e)
+        failed += 1
+   
+    end = datetime.now()
+ 
+    end_time = end.strftime("%Y-%m-%dT%H:%M:%S.%f %Z")
+    # Stop Timer
+    s_stop = timer()
+    s_runtime = "%.2f" % (1000 * (s_stop - s_start))
+ 
+    if success:
+        print("status=success remote_host=%s port=%s tcp_seq=%s time=%s ms start=%s end=%s" % (host, port, (count-1), s_runtime, start_time, end_time))
+        passed += 1
+    else:
+        print("status=failure remote_host=%s port=%s tcp_seq=%s time=%s ms start=%s end=%s" % (host, port, (count-1), s_runtime, start_time, end_time))
+    # if (1000 * (s_stop - s_start)) > 300:
+    #     print("Connection to %s[%s]: tcp_seq=%s time=%s ms start=%s end=%s" % (host, port, (count-1), s_runtime, start_time, end_time))
+ 
+    # Sleep for 1sec
+    if count < maxCount:
+        time.sleep(1)
+ 
+# Output Results if maxCount reached
+# getResults()
+ 
+ 
